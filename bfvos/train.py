5728ff292c76b3452c28d1a4e4dbb34cf23eba61
Suraj M S <braindeadpool@gmail.com>
2018-05-21 17:58:15 +0100

    added training and logging code

--- bfvos/train.py
+++ bfvos/train.py
@@ -1,14 +1,38 @@
 import os
+import numpy as np
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
 from .dataset import davis
 from .model import network, loss, config
+import logging
+import time
 
+# Logging setup
+logging.basicConfig()
+logger = logging.getLogger()
+
+# Set paths
 root_dir = os.path.dirname(__file__)
+model_dir = os.path.join(root_dir, 'model')
+checkpoint_dir = os.path.join(model_dir)
+config_save_dir = os.path.join(model_dir)
+
+# Intervals
+log_interval = 10
+checkpoint_interval = 10
+
+# Pytorch configs
+device = "cpu"
+# device = "cuda"
 torch.set_default_tensor_type(config.DEFAULT_TENSOR_TYPE)
 torch.set_default_dtype(config.DEFAULT_DTYPE)
+torch.device(device)
+seed = None
+if seed is not None:
+    np.random.seed(seed)
+    torch.manual_seed(seed)
 
 # Model configuration
 image_width = 50
@@ -24,7 +48,6 @@ use_cuda = False
 num_anchor_sample_points = 256  # according to paper
 alpha = 1  # slack variable for loss
 
-
 def main():
     data_source = davis.DavisDataset(base_dir=os.path.join(root_dir, 'dataset', 'DAVIS'),
                                      image_size=(image_width, image_height), year=2016, phase='train',
@@ -37,11 +60,20 @@ def main():
     optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
 
     for epoch in range(num_epochs):
-        print("Epoch {}/{}".format(epoch + 1, num_epochs))
-        train(data_loader, model, loss_fn, optimizer)
+        logger.info("Epoch {}/{}".format(epoch + 1, num_epochs))
+        train(data_loader, model, loss_fn, optimizer, epoch)
+
+    # Save final model
+    model.eval().cpu()
+    save_model_filename = "epoch_{}_{}.model".format(num_epochs, str(time.time()).replace(" ", "_"))
+    save_model_path = os.path.join(model_dir, save_model_filename)
+    torch.save(model.state_dict(), save_model_path)
+    logger.info("Model saved to {}".format(save_model_filename))
 
 
-def train(data_loader, model, loss_fn, optimizer):
+def train(data_loader, model, loss_fn, optimizer, epoch):
+    agg_fg_loss = 0.
+    agg_bg_loss = 0.
     for idx, sample in enumerate(data_loader):
         sample_frames = torch.autograd.Variable(sample['image'])
         embeddings = model(sample_frames)
@@ -79,15 +111,31 @@ def train(data_loader, model, loss_fn, optimizer):
 
         fg_loss = loss_fn(fg_embedding_a, fg_positive_pool, fg_negative_pool)
         bg_loss = loss_fn(bg_embedding_a, bg_positive_pool, bg_negative_pool)
-
         final_loss = fg_loss + bg_loss
-        print("Loss = {}".format(final_loss))
+
+        # Backpropagation
         optimizer.zero_grad()
         final_loss.backward()
         optimizer.step()
 
-        if idx == 2:
-            break
+        # Logging
+
+        agg_fg_loss += fg_loss.item()
+        agg_bg_loss += bg_loss.item()
+        if (idx + 1) % log_interval == 0:
+            logger.info("Epoch: {}, Batch: {}".format(epoch + 1, idx + 1))
+            logger.info("Avg FG Loss: {}, Avg BG Loss: {}, Avg Total Loss: {}".format(agg_fg_loss / (idx + 1),
+                                                                                      agg_bg_loss / (idx + 1),
+                                                                                      (agg_fg_loss + agg_bg_loss) / (
+                                                                                          idx + 1)))
+
+        if (idx + 1) % checkpoint_interval == 0:
+            model.eval().cpu()
+            ckpt_filename = "ckpt_epoch_{}_batch_id_{}.pth".format(epoch + 1, idx + 1)
+            ckpt_path = os.path.join(checkpoint_dir, ckpt_filename)
+            torch.save(model.state_dict(), ckpt_path)
+            logger.info("Checkpoint saved at {}".format(ckpt_filename))
+            model.to(device).train()
 
 
 if __name__ == "__main__":
