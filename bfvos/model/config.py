ef3ab88cd13f9646e2d8aa8d83886e831fcd47d0
Suraj M S <braindeadpool@gmail.com>
2018-05-23 20:49:45 +0100

    update cuda loading

--- bfvos/model/config.py
+++ bfvos/model/config.py
@@ -1,4 +1,8 @@
 import torch
 
-DEFAULT_TENSOR_TYPE = torch.DoubleTensor
-DEFAULT_DTYPE = torch.double
+if torch.cuda.is_available():
+    DEFAULT_TENSOR_TYPE = torch.cuda.DoubleTensor
+    DEFAULT_DTYPE = torch.cuda.double
+else:
+    DEFAULT_TENSOR_TYPE = torch.DoubleTensor
+    DEFAULT_DTYPE = torch.double
