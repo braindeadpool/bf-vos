53bc00509d9aa403fba497ad1bcf63d38974991a
Suraj M S <braindeadpool@gmail.com>
2018-05-28 07:01:59 +0100

    change val loss to not blow up gpu memory

--- bfvos/model/loss.py
+++ bfvos/model/loss.py
@@ -42,13 +42,14 @@ def validation_loss(anchor_points, positive_pool, negative_pool):
     :param negative_pool: Lxd tensor representing L negative pool points
     :return: float validation loss
     """
+    # TODO: Find replacement for repeat() since it blows up GPU memory even in Pytorch eval mode.
     positive_distances = distance_matrix(anchor_points, positive_pool)  # N x M
     negative_distances = distance_matrix(anchor_points, negative_pool)  # N x L
     N, M = positive_distances.size()
     N, L = negative_distances.size()
     p_ = positive_distances.repeat(1, L)  # N x (M*L)
     n_ = negative_distances.repeat(1, M)  # N x (M*L)
-    # For each anchor point, for each combination of positive, negative pair, count how many pairs exist
+    # For each anchor point, for each combination of positive, negative pair, count fraction of pairs
     # where the positive point is farther than negative point
     return torch.sum(torch.gt(p_, n_)).float() / float(N * M * L)
 
--- bfvos/train.py
+++ bfvos/train.py
@@ -258,7 +258,8 @@ def train(epoch, train_data_loader, val_data_loader, model, train_loss_fn, val_l
         if (idx + 1) % val_interval == 0:
             model.freeze_bn()
             model.eval()
-            validate(epoch, val_data_loader, model, val_loss_fn, val_loss_meter, summary_writer, num_val_batches)
+            # validate(epoch, val_data_loader, model, val_loss_fn, val_loss_meter, summary_writer, num_val_batches)
+            validate(epoch, val_data_loader, model, train_loss_fn, val_loss_meter, summary_writer, num_val_batches)
             model.train()
             model.freeze_feature_extraction()
 
